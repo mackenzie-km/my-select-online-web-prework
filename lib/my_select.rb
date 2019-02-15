0e69e2bad0098403fe1e5fb203b08568869e347f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-15 23:20:03 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,9 @@ def my_select(collection)
 i = 0
 new_array = []
 while i < collection.length 
-if collection[i] == true
-  yield collection[i]
+ yield collection[i]
+ if collection[i] == true
+ new_array << collection[i]
 end
 i+=1
  end
